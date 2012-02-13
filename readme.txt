2afe63b2d26652f3655f826dcf4f7fdeff787027
cd34 <cd34@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-13 16:26:36 +0000

    Initial import
    git-svn-id: https://plugins.svn.wordpress.org/snapreplay/trunk@504373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Plugin Name: SnapReplay
+Donate: http://snapreplay.com/
+Contributors: cd34
+Tags: 
+Requires at least: 2.6
+Tested up to: 3.3
+Stable tag: 0.1
+
+This widget allows you to specify an event or venue on SnapReplay to follow and publishes the latest picture.
+
+== Description ==
+
+This plugin uses [SnapReplay](http://snapreplay.com/) 
+
+== Installation ==
+
+1. Activate Plugin
+2. Appearance, Widgets, drag the SnapReplay Widget to the desired Sidebar
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. 
+
+2. 
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
+
+== Upgrade Notice ==
+
---
+++ snapreplay.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: SnapReplay
+Plugin URI: http://code.google.com/p/wordpress-snapreplay/
+Description: Display latest Event/Venue picture in sidebar
+Author: Chris Davies
+Version: 0.1
+Author URI: http://cd34.com/
+
+Update Event Title on page
+
+*/
+
+function widget_sr_control() {
+?>
+<?php
+}
+
+function widget_sr($args) {
+?>
+<h3><a href="http://snapreplay.com/event_id/<?php echo get_option('snapreplay-stream-id'); ?>">SnapReplay Live Stream</a></h3>
+<div id="top_placeholder"></div>
+<?php
+}
+	
+function widget_sr_init() {
+  if ( !function_exists('register_sidebar_widget') ||
+       !function_exists('register_widget_control') ) {
+    return;
+  }
+
+  register_sidebar_widget('SnapReplay Widget', 'widget_sr');
+
+}
+
+function sr_widget_menu() {
+  add_options_page('SnapReplay Widget Options', 'SnapReplay Widget', 'manage_options', 'sr-widget-options', 'sr_widget_options');
+}
+
+function sr_widget_options() {
+    if (!current_user_can('manage_options'))  {
+        wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+?>
+
+<div class="wrap">
+<h2>SnapReplay Widget setup</h2>
+<p>
+<a href="http://snapreplay.com/">SnapReplay.com</a> is a site that allows
+you to crowdsource photos from events. This plugin allows you to select
+an Event or Venue and display the last updated stream item Live in your
+sidebar.
+</p>
+<p>
+You need to configure the Event or Venue ID so that the widget knows
+which stream to follow.
+</p>
+<form method="post" action="options.php">
+    <?php settings_fields( 'snapreplay' ); ?>
+
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Event or Venue ID</th>
+        <td><input type="text" name="snapreplay-stream-id" value="<?php echo get_option('snapreplay-stream-id'); ?>" /></td>
+        </tr>
+    </table>
+
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+<?php
+}
+
+function sr_widget_init() {
+  register_setting('snapreplay', 'snapreplay-stream-id', 'intval');
+  update_option('snapreplay-stream-id', 1);
+}
+
+add_action('init', 'widget_sr_init');
+
+add_action('admin_menu', 'sr_widget_menu');
+add_action('admin_init', 'sr_widget_init');
+
+add_action('wp_footer', 'sr_jscript');
+
+function sr_jscript() {
+?>
+<script type="text/javaScript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javaScript" src="http://stream.snapreplay.com/socket.io/socket.io.js"></script>
+<script type="text/javaScript">
+<!--
+  var socket = io.connect('http://stream.snapreplay.com');
+  socket.emit('newchan', {'chan':<?php echo get_option('snapreplay-stream-id'); ?>});
+  socket.on('s-<?php echo get_option('snapreplay-stream-id'); ?>', function (data) {
+    if (data['data'].content_type == "text") {
+      content = data['data'].display_name + ' says, ' + data['data'].content;
+      $('#top_placeholder').html(content);
+    }
+    if (data['data'].content_type == "image") {
+      content='<img src="http://csnap.colocdn.com/pics/' + data['data'].file_name + '"/>';
+      $('#top_placeholder').html(content);
+    }
+  });
+// -->
+</script>
+<?php
+}
